43a57d6756afa7ebdd67369a040622a4987d66d3
@Noch4nce <m4rgera123@mail.ru>
2020-11-28 20:30:49 +0500

    feat: add data, ui

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 import '../styles/scss/main.scss';
+import cardData from './cards';
+
+console.log(cardData);
+document.body.insertAdjacentHTML('beforeend', `<img src="${cardData[8][0].image}">`)
+const ready = (callback) => {
+    if (document.readyState !== 'loading') callback();
+    else document.addEventListener('DOMContentLoaded', callback);
+};
+
+ready(() => {
+    document.querySelector('.first-button').addEventListener('click', () => {
+        document.querySelector('.animated-icon1').classList.toggle('open');
+    });
+});
