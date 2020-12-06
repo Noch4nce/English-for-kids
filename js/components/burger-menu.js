cb5228cd46c68f32c77fa3ca2890470117668ce2
@Noch4nce <m4rgera123@mail.ru>
2020-12-06 19:43:27 +0500

    feat: overlay, navClick, nav-styles

--- js/components/burger-menu.js
+++ js/components/burger-menu.js
@@ -3,14 +3,27 @@ const ready = (callback) => {
     else document.addEventListener('DOMContentLoaded', callback);
 };
 
+const navButton = document.querySelector('.navbar-toggler');
+const navBar = document.querySelector('.navbar-nav');
+const overlay = document.querySelector('.overlay');
+
 ready(() => {
-    document.querySelector('.first-button').addEventListener('click', () => {
-        document.querySelector('.animated-icon1').classList.toggle('open');
-    });
     document.querySelectorAll('.nav-link').forEach((element) => {
         element.addEventListener('click', () => {
             document.querySelector('.animated-icon1').classList.remove('open');
             document.querySelector('.show').classList.remove('show');
+            document.querySelector('.navbar-nav').classList.remove('nav-active');
+            document.querySelector('.overlay_active').classList.remove('overlay_active');
         });
     });
 });
+
+const navClick = () => {
+    navBar.classList.toggle('nav-active');
+    overlay.classList.toggle('overlay_active');
+    document.querySelector('.animated-icon1').classList.toggle('open');
+    document.querySelector('.show').classList.toggle('show');
+};
+
+navButton.addEventListener('click', () => navClick());
+overlay.addEventListener('click', () => navClick());
